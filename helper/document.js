ea93d838375018ab431ae2108f0169fd2cfe142e
nealcaffre <31470135+nealcaffre@users.noreply.github.com>
2022-02-05 23:03:06 +0530

    changes done for sidebar

---
+++ helper/document.js
@@ -0,0 +1,10 @@
+const sidebar =document.querySelector(".sidebar");
+const menuIcon =document.querySelector(".menu-icon-btn");
+// menuIcon.addEventListener("click",function(){
+//     sidebar.classList.add("icon-btn-focus");
+// })
+// document.documentElement.addEventListener("click",function(e){
+//     if(!e.target.classList.contains("menu-icon-btn")){
+//         sidebar.classList.remove("icon-btn-focus");
+//     }
+// })
